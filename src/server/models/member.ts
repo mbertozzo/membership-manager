d178ffc60f090212ab2898c2a5034149a8f28c6b
Matteo Bertozzo <matteobertozzo@outlook.com>
2023-07-27 21:51:54 +0200

    Add member model

--- src/server/models/member.ts
+++ src/server/models/member.ts
@@ -1,6 +1,6 @@
 export default (sequelize, DataTypes) => {
-  const Org = sequelize.define(
-    'org',
+  const Member = sequelize.define(
+    'member',
     {
       id: {
         type: DataTypes.INTEGER,
@@ -8,15 +8,15 @@ export default (sequelize, DataTypes) => {
         autoIncrement: true,
       },
       name: DataTypes.STRING,
-      description: DataTypes.STRING,
+      surname: DataTypes.STRING,
+      email: DataTypes.STRING,
     },
     {
       freezeTableName: true,
     },
   );
 
-  Org.associate = models => Org.belongsToMany(models.user, { through: models.user_org });
-  Org.associate = models => Org.hasMany(models.member);
+  Member.associate = models => Member.belongsTo(models.org);
 
-  return Org;
+  return Member;
 };
